349715426d1e12de2549565b1b422e9bc064ecdf
Heril Saha <herilsaha06@gmail.com>
2020-11-17 14:29:59 -0700

    final commit to assignment

--- HTML-CSS-BASICS/Assignment/index.js
+++ HTML-CSS-BASICS/Assignment/index.js
@@ -6,13 +6,8 @@ function onSubmit(e) {
   const q4 = document.getElementById("question4")
   const q5 = document.getElementById("question5")
 
-  // Error Handlers
-  const q1Error = document.getElementById("errorHandlingQ1");
-  const q2Error = document.getElementById("errorHandlingQ2");
-  const q3Error = document.getElementById("errorHandlingQ3");
-  const q4Error = document.getElementById("errorHandlingQ4");
-  const q5Error = document.getElementById("errorHandlingQ5");
-
+  // Mark
+  const mark = document.getElementById("mark")
   // Counter to know which questions user got correct
   let counter = 0;
 
@@ -25,30 +20,28 @@ function onSubmit(e) {
     q5: "lodash",
   };
 
+  // Adding 1 to counter if correct and ignoring case sensitivity
   if (q1.value.toLowerCase() === answers.q1) {
     counter += 1;
-  } else {
-    q1Error.innerHTML = "The Correct Answer was Machine Code";
   }
 
   if (q2.value.toLowerCase() === answers.q2) {
     counter += 1;
-  } else {
-  }
+  } 
 
   if (q3.value.toLowerCase() === answers.q3) {
     counter += 1;
-  } else {
   }
 
   if (q4.value.toLowerCase() === answers.q4) {
     counter += 1;
-  } else {
   }
 
-  if (q5.toLowerCase() === answers.q5) {
+  if (q5.value.toLowerCase() === answers.q5) {
     counter += 1;
-  } else {
   }
-  
+
+  // Percentage Calculation
+  const percent = counter / 5 * 100
+  mark.innerHTML = `Your Mark Is ${counter} / 5 (${percent}%)`
 }
