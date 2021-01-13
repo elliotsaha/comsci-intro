3002823cd38ed7ac20185d45540215ed752ba54a
HerilS <herilsaha06@gmail.com>
2021-01-12 21:04:52 -0700

    create tab system

--- finalproject/javascript/webComponents/index.js
+++ finalproject/javascript/webComponents/index.js
@@ -1,38 +1,51 @@
 var featuredGame = document.getElementById("featuredGame");
-var featuredGameTitle = document.getElementById("featuredGameTitle")
+var featuredGameTitle = document.getElementById("featuredGameTitle");
 
-// Tabs
-var homepage = document.getElementById("homepage")
-var arcade = document.getElementById("arcade")
+// Tab Elements
+var homepage = document.getElementById("homepage");
+var arcade = document.getElementById("arcade");
 
 // Tab Links
-var arcadeLink = document.getElementById("arcadeLink")
-
-var homepageLink = document.getElementById("homepageLink")
+var arcadeLink = document.getElementById("arcadeLink");
+var homepageLink = document.getElementById("homepageLink");
 
 // Change Featured Game Title on Mouse Hover
 featuredGame.addEventListener("mouseover", function () {
-  featuredGameTitle.innerHTML = "The Best Platformer"
-})
+  featuredGameTitle.innerHTML = "The Best Platformer";
+});
 
 featuredGame.addEventListener("mouseleave", function () {
-  featuredGameTitle.innerHTML = "Our Featured Game"
-})
+  featuredGameTitle.innerHTML = "Our Featured Game";
+});
 
-// Tab Content Opener
+// Tabs
+let activeTab = "homepage";
+var tabcontent = document.getElementsByClassName("tabContent");
+
+function changeTab() {
+  // Scroll to top of the screen
+  document.documentElement.scrollTop = 0;
+  // loop through tabcontent array
+  for (var i = 0; i < tabcontent.length; i++) {
+    // if tabcontent iterable id not equal to active tab: set to display none
+    if (tabcontent[i].id !== activeTab) {
+      tabcontent[i].style.display = "none";
+    } else {
+      // if active tab === tab content iterable: set to display block
+      tabcontent[i].style.display = "block";
+    }
+  }
+}
+
+// Set to only one tab during initialization
+changeTab();
 
 homepageLink.addEventListener("click", function () {
-  homepage.classList.remove("tabContent")
-  homepage.classList.add("tabContentActive")
-  arcade.classList.add("tabContent")
-  arcade.classList.remove("tabContentActive")
-  console.log(homepage.classList)
-})
+  activeTab = "homepage";
+  changeTab();
+});
 
-arcadeLink.addEventListener("click", function  () {
-  arcade.classList.remove("tabContent")
-  arcade.classList.add("tabContentActive")
-  homepage.classList.add("tabContent")
-  homepage.classList.remove("tabContentActive")
-  console.log(arcade.classList)
-})
+arcadeLink.addEventListener("click", function () {
+  activeTab = "arcade";
+  changeTab();
+});
